99e007fdd0f3fdc3ac6a6977809220bda6ad27ab
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-05-30 00:43:46 +0600

    add context function

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react'
+import { createContext,useState } from 'react';
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({});
+const URL = 'http://localhost:3000/products';
+  const [products, setProducts] = useState([]);
+ useEffect(() => {
+     const fetchData = async () => {
+       try {
+         const response = await fetch(URL);
+         if (!response.ok) {
+           throw new Error(`HTTP error! status: ${response.status}`);
+         }
+         const singleProduct = await response.json();
+         setProducts(singleProduct);
+       } catch (error) {
+         console.error("Failed to fetch products:", error);
+       }      
+     };
+     fetchData();
+   }, []);
+    const addToCart = (itemId) => {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: (prev[itemId] || 0) + 1,
+      }));
+    };
+    const removeFromCart = (itemId) => {
+      setCartItems((prev) => {
+        if (!prev[itemId]) return prev; 
+
+        const updatedCart = { ...prev };
+        if (updatedCart[itemId] === 1) {
+          delete updatedCart[itemId];
+        } else {
+          updatedCart[itemId] -= 1;
+        }
+        return updatedCart;
+      });
+    };
+    const getTotalCartAmount = () => {
+      return Object.entries(cartItems).reduce((total, [itemId, quantity]) => {
+        const itemInfo = products.find(
+          (product) => String(product.id) === String(itemId)
+        );
+        return itemInfo ? total + itemInfo.price * quantity : total;
+      }, 0);
+    };
+    useEffect(() => {
+      console.log(cartItems);
+    }, [cartItems]);
+    const contextValue = {
+        products,
+      cartItems,
+      addToCart,
+      removeFromCart,
+      getTotalCartAmount,
+    };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-// import StoreContextProvider from "./context/StoreContext.jsx";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
       <BrowserRouter>
-        {/* <StoreContextProvider> */}
+        <StoreContextProvider>
         <App />
-        {/* </StoreContextProvider> */}
+        </StoreContextProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>
