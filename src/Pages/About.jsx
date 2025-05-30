f0cafafcc80d516bf6fb3cea0ca8646bb377b676
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-05-30 20:41:25 +0600

    add about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import React from "react";
+import AboutImg from "../assets/about image.png";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className='container '>
+        <div className='block my-25 lg:flex justify-between items-center gap-20 relative'>
+          <img src={AboutImg} alt='' className='w-[45%] sticky' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat
+            quam reprehenderit natus, nobis consequuntur, minus quas error
+            mollitia accusantium dolorum ut odio repudiandae nesciunt ratione
+            nisi iure aliquam ipsam officiis sint sapiente suscipit? Aliquam
+            natus alias quasi iste optio quae eos reprehenderit quos id quaerat!
+            Tempora consequuntur aliquid repudiandae officiis ipsa aspernatur
+            deserunt possimus laboriosam similique, libero earum repellendus
+            provident quae error nemo quis ipsum alias beatae. Quisquam ducimus
+            aliquid mollitia temporibus cupiditate vitae expedita! Dolorum,
+            officia numquam, distinctio eveniet asperiores necessitatibus nihil
+            vel quo nobis, harum tempore a! Illum, ullam omnis. Harum fugiat
+            deleniti quia ipsa a rerum placeat repudiandae voluptatem pariatur
+            nam veniam maxime ullam voluptate voluptatum totam, quas similique?
+            Necessitatibus numquam fugit ullam corrupti rem, laudantium voluptas
+            totam nemo, repellat molestiae placeat. Incidunt asperiores quae sit
+            saepe minima molestiae, minus possimus necessitatibus dolores omnis
+            nobis quibusdam illo assumenda excepturi commodi voluptatem soluta
+            recusandae hic sed praesentium facilis. <br />
+            <br /> Minima recusandae, quos saepe natus optio officiis
+            dignissimos quae ratione asperiores blanditiis alias tempore
+            adipisci. Dolorum dolorem laboriosam vel animi at doloremque
+            aspernatur sequi quod ullam corrupti molestias tempora, nemo error?
+            Rerum doloremque voluptatibus cupiditate adipisci dignissimos, dicta
+            minus eos vel voluptas perferendis enim! Ducimus beatae amet
+            accusamus laborum sapiente minus ad, ipsum ullam assumenda minima
+            necessitatibus similique tenetur repellat dolor corporis ut
+            molestias voluptates earum laudantium fugit nisi? Repudiandae
+            dolorem culpa architecto rem odit, provident quasi consequatur
+            exercitationem optio repellat sint laudantium mollitia nemo,
+            explicabo inventore similique quae itaque veritatis facilis.
+            Perspiciatis totam magnam assumenda quasi ipsam veniam ea quod in
+            similique saepe praesentium dolore minima deleniti repellat enim,
+            laboriosam ut repudiandae, laudantium animi doloremque laborum? Sunt
+            doloribus incidunt delectus itaque vel alias minima nihil quis
+            veniam illum atque, minus quos rem, a omnis error explicabo magni
+            adipisci voluptatum aliquam beatae nisi! Numquam temporibus, dolorum
+            fugiat doloribus officiis quis?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
